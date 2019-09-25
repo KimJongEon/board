c0a9124208b5871ed3dbc869de36073da8091b67
KimJongEon <KimJongEon@DESKTOP-FG3R91Q>
2019-09-25 18:46:17 +0900

    2019/09/25
    로그인, 회원가입, 글 목록 뷰 완성

--- board/src/main/java/com/example/board/controller/HomeController.java
+++ board/src/main/java/com/example/board/controller/HomeController.java
@@ -16,31 +16,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+
+	// 로그인 페이지로 이동 (홈)
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
 		return "home";
 	}
-	
-	@RequestMapping(value = "/register", method = RequestMethod.GET)
+
+	// 회원가입 페이지로 이동
+	@RequestMapping(value = "/signUp", method = RequestMethod.GET)
 	public String register(Model model) {
-		
-		return "register";
-		
+
+		return "signUp";
+
 	}
-	
+
+	// 회원가입 페이지로 이동
+	@RequestMapping(value = "/postList", method = RequestMethod.GET)
+	public String postList(Model model) {
+
+		return "board/postList";
+
+	}
+
 }
