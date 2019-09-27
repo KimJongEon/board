7a4132ada7bc173708a1644300beb0bc5dcb61ee
KimJongEon <KimJongEon@DESKTOP-FG3R91Q>
2019-09-27 18:04:35 +0900

    2019/09/27
    로그인 구현 중

--- board/src/main/java/com/example/board/controller/LogInController.java
+++ board/src/main/java/com/example/board/controller/LogInController.java
@@ -7,13 +7,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.service.LogInService;
+
 @Controller
 public class LogInController {
+	private LogInService logInService;
+	
 	// 로그인 페이지로 이동
-	@RequestMapping(value = "/logIn", method = RequestMethod.GET)
-	public String logIn(Locale locale, Model model) {
+	@RequestMapping(value = "/logInPage", method = RequestMethod.GET)
+	public String logInPage(Locale locale, Model model) {
 
-		return "logIn/logIn";
+		return "logIn/logInPage";
 	}
+	
+	// 로그인
+	@RequestMapping(value = "/logIn", method = RequestMethod.POST)
+	public String logIn(Locale locale, Model model) {
+		
+		
+		return "post/postList";
+	}
+	
+	
 
 }
---
+++ board/src/main/java/com/example/service/LogInServiceImplement.java
@@ -0,0 +1,5 @@
+package com.example.service;
+
+public class LogInServiceImplement implements LogInService {
+
+}
